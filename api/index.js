2de0f264c127588d95a87f63553059144c5024c1
Rendy <rendyharvest@gmail.com>
2025-05-30 04:32:56 +0700

    bisa pls

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
-// import app from "./app.js";
-// import serverless from "serverless-http";
+import app from "./app.js";
+import serverless from "serverless-http";
 
-// const handler = serverless(app);
+const handler = serverless(app);
 
-// export default handler = async (event, context) => {
-//   const result = await handler(event, context);
-//   return result;
-// };
+export default handler = async (event, context) => {
+  const result = await handler(event, context);
+  return result;
+};
