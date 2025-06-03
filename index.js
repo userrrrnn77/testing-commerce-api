aa6b13cd66bc0ddc81f3a02d65d234152389e76c
Renss <165572436+userrrrnn77@users.noreply.github.com>
2025-06-04 00:09:49 +0700

    updated

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
-import app from "./api/app.js";
-import serverless from "serverless-http";
+// api/app.js
 
-const serverlessHandler = serverless(app);
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import database from "./api/config/Database.js";
+import userRouter from "./api/routes/UserRoutes.js";
+import productRouter from "./api/routes/ProductRoutes.js";
 
-export default async function handler(event, context) {
-  return await serverlessHandler(event, context);
-}
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173/"],
+    credentials: true,
+    methods: ["GET", "POST", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", userRouter);
+app.use("/api", productRouter);
+app.use((req, res, next) => {
+  res.setTimeout(60000);
+  next();
+});
+
+app.get("/", async (req, res) => {
+  await database();
+  res.send(`Connceted Vercel`);
+});
+
+app.get("/vercel", async (req, res) => {
+  await database();
+  res.json({ message: "API is running on Vercel!" });
+});
+
+app.listen(5000, () => console.log(`server up and running at http://localhost:5000`))
+
+export default app;
